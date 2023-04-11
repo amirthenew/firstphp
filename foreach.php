459d5d18916ac08b0fa55fcfaaeadb405113aa91
amirthenew <ah.abshari@hotmail.com>
2023-04-11 23:50:07 +0430

    foreach

--- foreach.php
+++ foreach.php
@@ -7,3 +7,10 @@ $names = array('amir','ali','mahdi');
 foreach ($names as $value){
 echo $value.'<br>' ;
 }
+
+
+//you can set index for arrays;
+$names2 = array('user1'=>'amir','user2'=>'reza');
+foreach ($names2 as $value){
+    echo $value;
+}
